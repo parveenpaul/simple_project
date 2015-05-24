80fbdc51435c424d7e6b22567c5e1990829f2f42
parveen pal <paulparveen01@gmail.com>
2015-05-24 16:26:39 +0530

    Initial upload..

---
+++ composer/index.php
@@ -0,0 +1,10 @@
+<?php
+// load Composer
+require 'vendor/autoload.php';
+
+// create demo data
+$variable = array(1, 17, "hello", null, array(1, 2, 3));
+
+// use KINT directly (which has been loaded automatically via Composer)
+d($variable);
+?>
